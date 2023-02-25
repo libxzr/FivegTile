d554f1871b7ea88564819320ba355ba4e100fc24
LibXZR <i@xzr.moe>
2023-02-25 23:31:59 +0800

    Get rid of inline optimizations
    Well, this **may** break compatibility across devices and versions.

--- app/src/main/java/moe/xzr/fivegtile/FivegControllerService.kt
+++ app/src/main/java/moe/xzr/fivegtile/FivegControllerService.kt
@@ -5,9 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.ServiceManager
-import android.telephony.TelephonyManager
 import com.android.internal.telephony.ITelephony
-import com.android.internal.telephony.RILConstants
 import com.topjohnwu.superuser.ipc.RootService
 
 /**
@@ -19,19 +17,42 @@ class FivegControllerService : RootService() {
         private val iTelephony by lazy {
             ITelephony.Stub.asInterface(ServiceManager.getService(Context.TELEPHONY_SERVICE))
         }
+        private val reasonUser by lazy {
+            Class.forName("android.telephony.TelephonyManager")
+                .getDeclaredField("ALLOWED_NETWORK_TYPES_REASON_USER")
+                .getInt(null)
+        }
+        private val typeNr by lazy {
+            Class.forName("android.telephony.TelephonyManager")
+                .getDeclaredField("NETWORK_TYPE_BITMASK_NR")
+                .getLong(null)
+        }
+
+        @delegate:SuppressLint("PrivateApi", "BlockedPrivateApi")
+        private val modeLte by lazy {
+            Class.forName("com.android.internal.telephony.RILConstants")
+                .getDeclaredField("NETWORK_MODE_LTE_TDSCDMA_CDMA_EVDO_GSM_WCDMA")
+                .getInt(null)
+        }
+
+        @delegate:SuppressLint("PrivateApi", "BlockedPrivateApi")
+        private val modeNr by lazy {
+            Class.forName("com.android.internal.telephony.RILConstants")
+                .getDeclaredField("NETWORK_MODE_NR_LTE_TDSCDMA_CDMA_EVDO_GSM_WCDMA")
+                .getInt(null)
+        }
     }
 
     override fun onBind(intent: Intent) = object : IFivegController.Stub() {
-        @SuppressLint("InlinedApi")
         override fun compatibilityCheck(subId: Int): Boolean {
             return try {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
                     iTelephony.setAllowedNetworkTypesForReason(
                         subId,
-                        TelephonyManager.ALLOWED_NETWORK_TYPES_REASON_USER,
+                        reasonUser,
                         iTelephony.getAllowedNetworkTypesForReason(
                             subId,
-                            TelephonyManager.ALLOWED_NETWORK_TYPES_REASON_USER
+                            reasonUser
                         )
                     )
                 } else {
@@ -46,40 +67,38 @@ class FivegControllerService : RootService() {
             }
         }
 
-        @SuppressLint("InlinedApi")
         override fun getFivegEnabled(subId: Int): Boolean {
             return try {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
                     iTelephony.getAllowedNetworkTypesForReason(
                         subId,
-                        TelephonyManager.ALLOWED_NETWORK_TYPES_REASON_USER
-                    ) and TelephonyManager.NETWORK_TYPE_BITMASK_NR != 0L
+                        reasonUser
+                    ) and typeNr != 0L
                 } else {
                     // For Q and R.
                     iTelephony.getPreferredNetworkType(subId) ==
-                            RILConstants.NETWORK_MODE_NR_LTE_TDSCDMA_CDMA_EVDO_GSM_WCDMA
+                            modeNr
                 }
             } catch (_: Exception) {
                 false
             }
         }
 
-        @SuppressLint("InlinedApi")
         override fun setFivegEnabled(subId: Int, enabled: Boolean) {
             try {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
                     var curTypes = iTelephony.getAllowedNetworkTypesForReason(
                         subId,
-                        TelephonyManager.ALLOWED_NETWORK_TYPES_REASON_USER
+                        reasonUser
                     )
                     curTypes = if (enabled) {
-                        curTypes or TelephonyManager.NETWORK_TYPE_BITMASK_NR
+                        curTypes or typeNr
                     } else {
-                        curTypes and TelephonyManager.NETWORK_TYPE_BITMASK_NR.inv()
+                        curTypes and typeNr.inv()
                     }
                     iTelephony.setAllowedNetworkTypesForReason(
                         subId,
-                        TelephonyManager.ALLOWED_NETWORK_TYPES_REASON_USER,
+                        reasonUser,
                         curTypes
                     )
                 } else {
@@ -87,12 +106,12 @@ class FivegControllerService : RootService() {
                     if (enabled) {
                         iTelephony.setPreferredNetworkType(
                             subId,
-                            RILConstants.NETWORK_MODE_NR_LTE_TDSCDMA_CDMA_EVDO_GSM_WCDMA
+                            modeNr
                         )
                     } else {
                         iTelephony.setPreferredNetworkType(
                             subId,
-                            RILConstants.NETWORK_MODE_LTE_TDSCDMA_CDMA_EVDO_GSM_WCDMA
+                            modeLte
                         )
                     }
                 }
--- hiddenapi/src/main/java/com/android/internal/telephony/RILConstants.java
+++
@@ -1,6 +0,0 @@
-package com.android.internal.telephony;
-
-public interface RILConstants {
-    int NETWORK_MODE_LTE_TDSCDMA_CDMA_EVDO_GSM_WCDMA = 22;
-    int NETWORK_MODE_NR_LTE_TDSCDMA_CDMA_EVDO_GSM_WCDMA = 33;
-}
