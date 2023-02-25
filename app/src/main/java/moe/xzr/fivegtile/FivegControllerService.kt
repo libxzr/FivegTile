682f5d48fbddf13bf1e988c461fe46397753255e
LibXZR <i@xzr.moe>
2023-02-25 23:20:18 +0800

    Fix for Android R and add support for Android Q

--- app/src/main/java/moe/xzr/fivegtile/FivegControllerService.kt
+++ app/src/main/java/moe/xzr/fivegtile/FivegControllerService.kt
@@ -7,6 +7,7 @@ import android.os.Build
 import android.os.ServiceManager
 import android.telephony.TelephonyManager
 import com.android.internal.telephony.ITelephony
+import com.android.internal.telephony.RILConstants
 import com.topjohnwu.superuser.ipc.RootService
 
 /**
@@ -18,41 +19,28 @@ class FivegControllerService : RootService() {
         private val iTelephony by lazy {
             ITelephony.Stub.asInterface(ServiceManager.getService(Context.TELEPHONY_SERVICE))
         }
-
-        @SuppressLint("InlinedApi")
-        private fun getAllowedNetworkTypes(subId: Int): Long {
-            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                iTelephony.getAllowedNetworkTypesForReason(
-                    subId,
-                    TelephonyManager.ALLOWED_NETWORK_TYPES_REASON_USER
-                )
-            } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.R) {
-                iTelephony.getAllowedNetworkTypes(subId)
-            } else {
-                throw RuntimeException("not compatible")
-            }
-        }
-
-        @SuppressLint("InlinedApi")
-        private fun setAllowedNetworkTypes(subId: Int, types: Long): Boolean {
-            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                iTelephony.setAllowedNetworkTypesForReason(
-                    subId,
-                    TelephonyManager.ALLOWED_NETWORK_TYPES_REASON_USER,
-                    types
-                )
-            } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.R) {
-                iTelephony.setAllowedNetworkTypes(subId, types)
-            } else {
-                throw RuntimeException("not compatible")
-            }
-        }
     }
 
     override fun onBind(intent: Intent) = object : IFivegController.Stub() {
+        @SuppressLint("InlinedApi")
         override fun compatibilityCheck(subId: Int): Boolean {
             return try {
-                setAllowedNetworkTypes(subId, getAllowedNetworkTypes(subId))
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    iTelephony.setAllowedNetworkTypesForReason(
+                        subId,
+                        TelephonyManager.ALLOWED_NETWORK_TYPES_REASON_USER,
+                        iTelephony.getAllowedNetworkTypesForReason(
+                            subId,
+                            TelephonyManager.ALLOWED_NETWORK_TYPES_REASON_USER
+                        )
+                    )
+                } else {
+                    // For Q and R.
+                    iTelephony.setPreferredNetworkType(
+                        subId,
+                        iTelephony.getPreferredNetworkType(subId)
+                    )
+                }
             } catch (_: Exception) {
                 false
             }
@@ -61,7 +49,16 @@ class FivegControllerService : RootService() {
         @SuppressLint("InlinedApi")
         override fun getFivegEnabled(subId: Int): Boolean {
             return try {
-                getAllowedNetworkTypes(subId) and TelephonyManager.NETWORK_TYPE_BITMASK_NR != 0L
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    iTelephony.getAllowedNetworkTypesForReason(
+                        subId,
+                        TelephonyManager.ALLOWED_NETWORK_TYPES_REASON_USER
+                    ) and TelephonyManager.NETWORK_TYPE_BITMASK_NR != 0L
+                } else {
+                    // For Q and R.
+                    iTelephony.getPreferredNetworkType(subId) ==
+                            RILConstants.NETWORK_MODE_NR_LTE_TDSCDMA_CDMA_EVDO_GSM_WCDMA
+                }
             } catch (_: Exception) {
                 false
             }
@@ -70,13 +67,35 @@ class FivegControllerService : RootService() {
         @SuppressLint("InlinedApi")
         override fun setFivegEnabled(subId: Int, enabled: Boolean) {
             try {
-                var curTypes = getAllowedNetworkTypes(subId)
-                curTypes = if (enabled) {
-                    curTypes or TelephonyManager.NETWORK_TYPE_BITMASK_NR
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    var curTypes = iTelephony.getAllowedNetworkTypesForReason(
+                        subId,
+                        TelephonyManager.ALLOWED_NETWORK_TYPES_REASON_USER
+                    )
+                    curTypes = if (enabled) {
+                        curTypes or TelephonyManager.NETWORK_TYPE_BITMASK_NR
+                    } else {
+                        curTypes and TelephonyManager.NETWORK_TYPE_BITMASK_NR.inv()
+                    }
+                    iTelephony.setAllowedNetworkTypesForReason(
+                        subId,
+                        TelephonyManager.ALLOWED_NETWORK_TYPES_REASON_USER,
+                        curTypes
+                    )
                 } else {
-                    curTypes and TelephonyManager.NETWORK_TYPE_BITMASK_NR.inv()
+                    // For Q and R.
+                    if (enabled) {
+                        iTelephony.setPreferredNetworkType(
+                            subId,
+                            RILConstants.NETWORK_MODE_NR_LTE_TDSCDMA_CDMA_EVDO_GSM_WCDMA
+                        )
+                    } else {
+                        iTelephony.setPreferredNetworkType(
+                            subId,
+                            RILConstants.NETWORK_MODE_LTE_TDSCDMA_CDMA_EVDO_GSM_WCDMA
+                        )
+                    }
                 }
-                setAllowedNetworkTypes(subId, curTypes)
             } catch (_: Exception) {
 
             }
--- hiddenapi/src/main/aidl/com/android/internal/telephony/ITelephony.aidl
+++ hiddenapi/src/main/aidl/com/android/internal/telephony/ITelephony.aidl
@@ -3,6 +3,6 @@ package com.android.internal.telephony;
 interface ITelephony {
     long getAllowedNetworkTypesForReason(int subId, int reason);
     boolean setAllowedNetworkTypesForReason(int subId, int reason, long allowedNetworkTypes);
-    long getAllowedNetworkTypes(int subId);
-    boolean setAllowedNetworkTypes(int subId, long allowedNetworkTypes);
+    int getPreferredNetworkType(int subId);
+    boolean setPreferredNetworkType(int subId, int networkType);
 }
